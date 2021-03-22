ed23e0838743831e7258d79b1f1a04c51ff980ec
PC <jucampana@alumno.unlam.edu.ar>
2021-03-22 13:33:03 -0300

    Agregado Club+Triatleta

--- src/ar/edu/unlam/pb2/eva03/Club.java
+++ src/ar/edu/unlam/pb2/eva03/Club.java
@@ -2,11 +2,27 @@ package ar.edu.unlam.pb2.eva03;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Club {
 
 	private String nombre;
 	private Set<Deportista> socios;
 	private Map<String, Evento> competencias;
+	
+	public Club(String nombre) {
+		this.nombre=nombre;
+		this.socios=new TreeSet<Deportista>();
+		this.competencias=new TreeMap<String, Evento>();
+	}
+	
+	public  void agregarDeportista(Deportista nuevo) {
+		socios.add(nuevo);
+	}
+	
+	public Integer getCantidadSocios() {
+		return socios.size();
+	}
 	
 }
