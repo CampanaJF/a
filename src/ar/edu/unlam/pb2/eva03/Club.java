a85106f34c75e0dea4cb192cf203607c63221968
PC <jucampana@alumno.unlam.edu.ar>
2021-03-22 15:11:24 -0300

    The End

--- src/ar/edu/unlam/pb2/eva03/Club.java
+++ src/ar/edu/unlam/pb2/eva03/Club.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import ar.edu.unlam.pb2.eva03.enumeradores.TipoDeEvento;
+
 public class Club {
 
 	private String nombre;
@@ -23,6 +25,114 @@ public class Club {
 	
 	public Integer getCantidadSocios() {
 		return socios.size();
+	}
+
+	public void crearEvento(TipoDeEvento tipoDeEvento, String descripcion) {
+		Evento eventoNuevo=new Evento(tipoDeEvento,descripcion);
+		competencias.put(descripcion, eventoNuevo);
+		
+	}
+
+	public Object inscribirEnEvento(String descripcion, Deportista deportista) throws NoEstaPreparado {
+		
+		Integer valor=0;
+		
+		switch(this.competencias.get(descripcion).getTipoDeEvento()) {
+		
+		case CARRERA_10K:
+			if(deportista instanceof Corredor|| deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+			break;
+		case CARRERA_21K:
+			if(deportista instanceof Corredor|| deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+			
+			break;
+		case CARRERA_42K:
+			if(deportista instanceof Corredor|| deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+		
+			break;
+		case CARRERA_5K:
+			if(deportista instanceof Corredor|| deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+		
+			break;
+		case CARRERA_NATACION_EN_AGUAS_ABIERTAS:
+			if(deportista instanceof Nadador|| deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+		
+			break;
+		case CARRERA_NATACION_EN_PICINA:
+			if(deportista instanceof Nadador|| deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+		
+			break;
+		case DUATLON:
+			if(deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+		
+			break;
+		case TRIATLON_IRONMAN:
+			if(deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+
+			break;
+		case TRIATLON_MEDIO:
+			if(deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+		
+			break;
+		case TRIATLON_OLIMPICO:
+			if(deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+		
+			break;
+		case TRIATLON_SHORT:
+			if(deportista instanceof Triatleta) {
+				competencias.get(descripcion).agregarParticipantes(deportista);}
+			else {
+				throw new NoEstaPreparado();}
+			valor=competencias.get(descripcion).getNumeroDeInscriptos();
+		
+			break;
+		
+		}
+		
+		  return valor;
+		
+		
+		
 	}
 	
 }
--- src/ar/edu/unlam/pb2/eva03/Evento.java
+++ src/ar/edu/unlam/pb2/eva03/Evento.java
@@ -4,13 +4,40 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import ar.edu.unlam.pb2.eva03.enumeradores.TipoDeEvento;
 
 public class Evento {
 
-	private TipoDeEvento tipo;
-	private Integer numeroDeInscripcion;
+	private TipoDeEvento tipoDeEvento;
+	private Integer numeroDeInscriptos;
 	private Map<Integer, Deportista> participantes;
+	private String descripcion;
+	
+	public Evento(TipoDeEvento tipoDeEvento, String descripcion) {
+		this.tipoDeEvento=tipoDeEvento;
+		this.descripcion=descripcion;
+		this.numeroDeInscriptos=0;
+		this.participantes=new TreeMap<Integer,Deportista>();
+		
+	}
+	
+	public TipoDeEvento getTipoDeEvento() {
+		return this.tipoDeEvento;
+	}
+	
+	public Integer getNumeroDeInscriptos() {
+		return this.numeroDeInscriptos;
+	}
+	
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+	
+	public void agregarParticipantes(Deportista deportista) {
+		this.participantes.put(this.numeroDeInscriptos++, deportista);
+	}
+	
 	
 }
